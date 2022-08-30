509ac18db943ccb059f4847f4e60c50d6a106136
Tyler Letendre <tylerletendre98@gmail.com>
2022-08-30 13:21:32 -0400

    init commit with header added

---
+++ portfolio-website/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/header/Header';
+
+function App() {
+  return (
+    <div className="App">
+        <div className='header'>
+          <Header/>
+        </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio-website/src/components/header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './header.css'
+
+function Header() {
+  return (
+    <div className='header-container'>
+        <div>
+            <ul className='list'>
+                <li>About Me</li>
+                <li>Projects</li>
+                <li>Contact Me</li>
+            </ul>
+        </div>
+    </div>
+  ) 
+}
+
+export default Header
